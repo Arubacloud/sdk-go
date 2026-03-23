2113e43455d17ae8159f6bcd67b56bb1cdf9405c
Antonisio PRUCOLI MARTINS <antonisio.prucolimar@consultant.aruba.it>
2026-03-23 12:55:06 +0100

    feat: multitenant layer for aruba client

---
+++ pkg/multitenant/multitenant.go
@@ -0,0 +1,128 @@
+package multitenant
+
+import (
+	"log"
+	"sync"
+	"time"
+
+	"github.com/Arubacloud/sdk-go/pkg/aruba"
+)
+
+type Multitenant interface {
+	New(tenant string) error
+	NewFromOptions(tenant string, options *aruba.Options) error
+	Add(tenant string, client aruba.Client)
+	Get(tenant string) (aruba.Client, bool)
+	MustGet(tenant string) aruba.Client
+	GetOrNil(tenant string) aruba.Client
+}
+
+type entry struct {
+	client    aruba.Client
+	lastUsage time.Time
+}
+
+type multitenant struct {
+	clients        map[string]*entry
+	defaultOptions *aruba.Options
+
+	lock sync.RWMutex
+}
+
+var _ Multitenant = (*multitenant)(nil)
+
+func New(options *aruba.Options) Multitenant {
+	return &multitenant{
+		clients:        make(map[string]*entry),
+		defaultOptions: options,
+	}
+}
+
+func (m *multitenant) New(tenant string) error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	c, err := aruba.NewClient(m.defaultOptions)
+	if err != nil {
+		return err
+	}
+
+	m.clients[tenant] = &entry{client: c, lastUsage: time.Now()}
+
+	return nil
+}
+
+func (m *multitenant) NewFromOptions(tenant string, options *aruba.Options) error {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	c, err := aruba.NewClient(options)
+	if err != nil {
+		return err
+	}
+
+	m.clients[tenant] = &entry{client: c, lastUsage: time.Now()}
+
+	return nil
+}
+
+func (m *multitenant) Add(tenant string, client aruba.Client) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	m.clients[tenant] = &entry{client: client, lastUsage: time.Now()}
+}
+
+func (m *multitenant) Get(tenant string) (aruba.Client, bool) {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	e, ok := m.clients[tenant]
+
+	if ok {
+		e.lastUsage = time.Now()
+	}
+
+	return e.client, ok
+}
+
+func (m *multitenant) MustGet(tenant string) aruba.Client {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	e, ok := m.clients[tenant]
+	if !ok {
+		log.Fatalf("client for tenant '%s' not found", tenant)
+	}
+
+	e.lastUsage = time.Now()
+
+	return e.client
+}
+
+func (m *multitenant) GetOrNil(tenant string) aruba.Client {
+	m.lock.RLock()
+	defer m.lock.RUnlock()
+
+	e, ok := m.clients[tenant]
+	if !ok {
+		return nil
+	}
+
+	e.lastUsage = time.Now()
+
+	return e.client
+}
+
+func (m *multitenant) CleanUp(from time.Duration) {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	cleanupTime := time.Now().Add(-1 * from)
+
+	for t, e := range m.clients {
+		if e.lastUsage.Before(cleanupTime) {
+			delete(m.clients, t)
+		}
+	}
+}
