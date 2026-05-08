44d2dcd5b395711bcac2ca4b99bf7ec82db804a3
Antonisio PRUCOLI MARTINS <antonisio.prucolimar@consultant.aruba.it>
2026-05-08 15:51:28 +0200

    feat(vpn-ike): introduce typed IKE crypto aliases and retrofit wrappers

--- pkg/types/network.vpn-tunnel.go
+++ pkg/types/network.vpn-tunnel.go
@@ -1,114 +1,124 @@
 package types
 
-// VPN Encryption algorithm constants for IKESettings.Encryption and ESPSettings.Encryption.
+// IKEEncryption is the encryption algorithm for IKESettings.Encryption.
+//
+// GET /providers/Aruba.Network/vpn-tunnels for the live catalog.
+type IKEEncryption string
+
 const (
-	VPNEncryptionAES128            = "aes128"
-	VPNEncryptionAES192            = "aes192"
-	VPNEncryptionAES256            = "aes256"
-	VPNEncryptionAES128CTR         = "aes128ctr"
-	VPNEncryptionAES192CTR         = "aes192ctr"
-	VPNEncryptionAES256CTR         = "aes256ctr"
-	VPNEncryptionAES128CCM64       = "aes128ccm64"
-	VPNEncryptionAES128CCM96       = "aes128ccm96"
-	VPNEncryptionAES128CCM128      = "aes128ccm128"
-	VPNEncryptionAES192CCM64       = "aes192ccm64"
-	VPNEncryptionAES192CCM96       = "aes192ccm96"
-	VPNEncryptionAES192CCM128      = "aes192ccm128"
-	VPNEncryptionAES256CCM64       = "aes256ccm64"
-	VPNEncryptionAES256CCM96       = "aes256ccm96"
-	VPNEncryptionAES256CCM128      = "aes256ccm128"
-	VPNEncryptionAES128GCM64       = "aes128gcm64"
-	VPNEncryptionAES128GCM96       = "aes128gcm96"
-	VPNEncryptionAES128GCM128      = "aes128gcm128"
-	VPNEncryptionAES192GCM64       = "aes192gcm64"
-	VPNEncryptionAES192GCM96       = "aes192gcm96"
-	VPNEncryptionAES192GCM128      = "aes192gcm128"
-	VPNEncryptionAES256GCM64       = "aes256gcm64"
-	VPNEncryptionAES256GCM96       = "aes256gcm96"
-	VPNEncryptionAES256GCM128      = "aes256gcm128"
-	VPNEncryptionAES128GMAC        = "aes128gmac"
-	VPNEncryptionAES192GMAC        = "aes192gmac"
-	VPNEncryptionAES256GMAC        = "aes256gmac"
-	VPNEncryption3DES              = "3des"
-	VPNEncryptionBlowfish128       = "blowfish128"
-	VPNEncryptionBlowfish192       = "blowfish192"
-	VPNEncryptionBlowfish256       = "blowfish256"
-	VPNEncryptionCamellia128       = "camellia128"
-	VPNEncryptionCamellia192       = "camellia192"
-	VPNEncryptionCamellia256       = "camellia256"
-	VPNEncryptionCamellia128CTR    = "camellia128ctr"
-	VPNEncryptionCamellia192CTR    = "camellia192ctr"
-	VPNEncryptionCamellia256CTR    = "camellia256ctr"
-	VPNEncryptionCamellia128CCM64  = "camellia128ccm64"
-	VPNEncryptionCamellia128CCM96  = "camellia128ccm96"
-	VPNEncryptionCamellia128CCM128 = "camellia128ccm128"
-	VPNEncryptionCamellia192CCM64  = "camellia192ccm64"
-	VPNEncryptionCamellia192CCM96  = "camellia192ccm96"
-	VPNEncryptionCamellia192CCM128 = "camellia192ccm128"
-	VPNEncryptionCamellia256CCM64  = "camellia256ccm64"
-	VPNEncryptionCamellia256CCM96  = "camellia256ccm96"
-	VPNEncryptionCamellia256CCM128 = "camellia256ccm128"
-	VPNEncryptionSerpent128        = "serpent128"
-	VPNEncryptionSerpent192        = "serpent192"
-	VPNEncryptionSerpent256        = "serpent256"
-	VPNEncryptionTwofish128        = "twofish128"
-	VPNEncryptionTwofish192        = "twofish192"
-	VPNEncryptionTwofish256        = "twofish256"
-	VPNEncryptionCAST128           = "cast128"
-	VPNEncryptionChaCha20Poly1305  = "chacha20poly1305"
+	IKEEncryptionAES128            IKEEncryption = "aes128"
+	IKEEncryptionAES192            IKEEncryption = "aes192"
+	IKEEncryptionAES256            IKEEncryption = "aes256"
+	IKEEncryptionAES128CTR         IKEEncryption = "aes128ctr"
+	IKEEncryptionAES192CTR         IKEEncryption = "aes192ctr"
+	IKEEncryptionAES256CTR         IKEEncryption = "aes256ctr"
+	IKEEncryptionAES128CCM64       IKEEncryption = "aes128ccm64"
+	IKEEncryptionAES128CCM96       IKEEncryption = "aes128ccm96"
+	IKEEncryptionAES128CCM128      IKEEncryption = "aes128ccm128"
+	IKEEncryptionAES192CCM64       IKEEncryption = "aes192ccm64"
+	IKEEncryptionAES192CCM96       IKEEncryption = "aes192ccm96"
+	IKEEncryptionAES192CCM128      IKEEncryption = "aes192ccm128"
+	IKEEncryptionAES256CCM64       IKEEncryption = "aes256ccm64"
+	IKEEncryptionAES256CCM96       IKEEncryption = "aes256ccm96"
+	IKEEncryptionAES256CCM128      IKEEncryption = "aes256ccm128"
+	IKEEncryptionAES128GCM64       IKEEncryption = "aes128gcm64"
+	IKEEncryptionAES128GCM96       IKEEncryption = "aes128gcm96"
+	IKEEncryptionAES128GCM128      IKEEncryption = "aes128gcm128"
+	IKEEncryptionAES192GCM64       IKEEncryption = "aes192gcm64"
+	IKEEncryptionAES192GCM96       IKEEncryption = "aes192gcm96"
+	IKEEncryptionAES192GCM128      IKEEncryption = "aes192gcm128"
+	IKEEncryptionAES256GCM64       IKEEncryption = "aes256gcm64"
+	IKEEncryptionAES256GCM96       IKEEncryption = "aes256gcm96"
+	IKEEncryptionAES256GCM128      IKEEncryption = "aes256gcm128"
+	IKEEncryptionAES128GMAC        IKEEncryption = "aes128gmac"
+	IKEEncryptionAES192GMAC        IKEEncryption = "aes192gmac"
+	IKEEncryptionAES256GMAC        IKEEncryption = "aes256gmac"
+	IKEEncryption3DES              IKEEncryption = "3des"
+	IKEEncryptionBlowfish128       IKEEncryption = "blowfish128"
+	IKEEncryptionBlowfish192       IKEEncryption = "blowfish192"
+	IKEEncryptionBlowfish256       IKEEncryption = "blowfish256"
+	IKEEncryptionCamellia128       IKEEncryption = "camellia128"
+	IKEEncryptionCamellia192       IKEEncryption = "camellia192"
+	IKEEncryptionCamellia256       IKEEncryption = "camellia256"
+	IKEEncryptionCamellia128CTR    IKEEncryption = "camellia128ctr"
+	IKEEncryptionCamellia192CTR    IKEEncryption = "camellia192ctr"
+	IKEEncryptionCamellia256CTR    IKEEncryption = "camellia256ctr"
+	IKEEncryptionCamellia128CCM64  IKEEncryption = "camellia128ccm64"
+	IKEEncryptionCamellia128CCM96  IKEEncryption = "camellia128ccm96"
+	IKEEncryptionCamellia128CCM128 IKEEncryption = "camellia128ccm128"
+	IKEEncryptionCamellia192CCM64  IKEEncryption = "camellia192ccm64"
+	IKEEncryptionCamellia192CCM96  IKEEncryption = "camellia192ccm96"
+	IKEEncryptionCamellia192CCM128 IKEEncryption = "camellia192ccm128"
+	IKEEncryptionCamellia256CCM64  IKEEncryption = "camellia256ccm64"
+	IKEEncryptionCamellia256CCM96  IKEEncryption = "camellia256ccm96"
+	IKEEncryptionCamellia256CCM128 IKEEncryption = "camellia256ccm128"
+	IKEEncryptionSerpent128        IKEEncryption = "serpent128"
+	IKEEncryptionSerpent192        IKEEncryption = "serpent192"
+	IKEEncryptionSerpent256        IKEEncryption = "serpent256"
+	IKEEncryptionTwofish128        IKEEncryption = "twofish128"
+	IKEEncryptionTwofish192        IKEEncryption = "twofish192"
+	IKEEncryptionTwofish256        IKEEncryption = "twofish256"
+	IKEEncryptionCAST128           IKEEncryption = "cast128"
+	IKEEncryptionChaCha20Poly1305  IKEEncryption = "chacha20poly1305"
 )
 
-// VPN Hash algorithm constants for IKESettings.Hash and ESPSettings.Hash.
+// IKEHash is the hash algorithm for IKESettings.Hash.
+type IKEHash string
+
 const (
-	VPNHashMD5        = "md5"
-	VPNHashMD5128     = "md5_128"
-	VPNHashSHA1       = "sha1"
-	VPNHashSHA1160    = "sha1_160"
-	VPNHashSHA256     = "sha256"
-	VPNHashSHA25696   = "sha256_96"
-	VPNHashSHA384     = "sha384"
-	VPNHashSHA512     = "sha512"
-	VPNHashAESXCBC    = "aesxcbc"
-	VPNHashAESCMAC    = "aescmac"
-	VPNHashAES128GMAC = "aes128gmac"
-	VPNHashAES192GMAC = "aes192gmac"
-	VPNHashAES256GMAC = "aes256gmac"
+	IKEHashMD5        IKEHash = "md5"
+	IKEHashMD5128     IKEHash = "md5_128"
+	IKEHashSHA1       IKEHash = "sha1"
+	IKEHashSHA1160    IKEHash = "sha1_160"
+	IKEHashSHA256     IKEHash = "sha256"
+	IKEHashSHA25696   IKEHash = "sha256_96"
+	IKEHashSHA384     IKEHash = "sha384"
+	IKEHashSHA512     IKEHash = "sha512"
+	IKEHashAESXCBC    IKEHash = "aesxcbc"
+	IKEHashAESCMAC    IKEHash = "aescmac"
+	IKEHashAES128GMAC IKEHash = "aes128gmac"
+	IKEHashAES192GMAC IKEHash = "aes192gmac"
+	IKEHashAES256GMAC IKEHash = "aes256gmac"
 )
 
-// VPN DH group constants for IKESettings.DHGroup.
+// IKEDHGroup is the Diffie-Hellman group for IKESettings.DHGroup.
+type IKEDHGroup string
+
 const (
-	VPNDHGroup1  = "1"
-	VPNDHGroup2  = "2"
-	VPNDHGroup5  = "5"
-	VPNDHGroup14 = "14"
-	VPNDHGroup15 = "15"
-	VPNDHGroup16 = "16"
-	VPNDHGroup17 = "17"
-	VPNDHGroup18 = "18"
-	VPNDHGroup19 = "19"
-	VPNDHGroup20 = "20"
-	VPNDHGroup21 = "21"
-	VPNDHGroup22 = "22"
-	VPNDHGroup23 = "23"
-	VPNDHGroup24 = "24"
-	VPNDHGroup25 = "25"
-	VPNDHGroup26 = "26"
-	VPNDHGroup27 = "27"
-	VPNDHGroup28 = "28"
-	VPNDHGroup29 = "29"
-	VPNDHGroup30 = "30"
-	VPNDHGroup31 = "31"
-	VPNDHGroup32 = "32"
+	IKEDHGroup1  IKEDHGroup = "1"
+	IKEDHGroup2  IKEDHGroup = "2"
+	IKEDHGroup5  IKEDHGroup = "5"
+	IKEDHGroup14 IKEDHGroup = "14"
+	IKEDHGroup15 IKEDHGroup = "15"
+	IKEDHGroup16 IKEDHGroup = "16"
+	IKEDHGroup17 IKEDHGroup = "17"
+	IKEDHGroup18 IKEDHGroup = "18"
+	IKEDHGroup19 IKEDHGroup = "19"
+	IKEDHGroup20 IKEDHGroup = "20"
+	IKEDHGroup21 IKEDHGroup = "21"
+	IKEDHGroup22 IKEDHGroup = "22"
+	IKEDHGroup23 IKEDHGroup = "23"
+	IKEDHGroup24 IKEDHGroup = "24"
+	IKEDHGroup25 IKEDHGroup = "25"
+	IKEDHGroup26 IKEDHGroup = "26"
+	IKEDHGroup27 IKEDHGroup = "27"
+	IKEDHGroup28 IKEDHGroup = "28"
+	IKEDHGroup29 IKEDHGroup = "29"
+	IKEDHGroup30 IKEDHGroup = "30"
+	IKEDHGroup31 IKEDHGroup = "31"
+	IKEDHGroup32 IKEDHGroup = "32"
 )
 
-// VPN DPD action constants for IKESettings.DPDAction.
+// IKEDPDAction is the Dead Peer Detection action for IKESettings.DPDAction.
+type IKEDPDAction string
+
 const (
-	VPNDPDActionTrap    = "trap"
-	VPNDPDActionClear   = "clear"
-	VPNDPDActionRestart = "restart"
+	IKEDPDActionTrap    IKEDPDAction = "trap"
+	IKEDPDActionClear   IKEDPDAction = "clear"
+	IKEDPDActionRestart IKEDPDAction = "restart"
 )
 
-// VPN PFS group constants for ESPSettings.PFS.
+// VPN PFS group constants for ESPSettings.PFS (replaced by ESPPFSGroup in a later commit).
 const (
 	VPNPFSEnable    = "enable"
 	VPNPFSDisable   = "disable"
@@ -136,7 +146,6 @@ const (
 	VPNPFSDHGroup32 = "dh-group32"
 )
 
-// IPConfigurations contains network configuration of the VPN tunnel
 // SubnetInfo contains subnet CIDR and name for VPN tunnel IP configuration
 type SubnetInfo struct {
 	CIDR string `json:"cidr,omitempty"`
@@ -161,16 +170,16 @@ type IKESettings struct {
 	Lifetime int32 `json:"lifetime,omitempty"`
 
 	// Encryption Encryption algorithm (nullable)
-	Encryption *string `json:"encryption,omitempty"`
+	Encryption *IKEEncryption `json:"encryption,omitempty"`
 
 	// Hash Hash algorithm (nullable)
-	Hash *string `json:"hash,omitempty"`
+	Hash *IKEHash `json:"hash,omitempty"`
 
 	// DHGroup Diffie-Hellman group (nullable)
-	DHGroup *string `json:"dhGroup,omitempty"`
+	DHGroup *IKEDHGroup `json:"dhGroup,omitempty"`
 
 	// DPDAction Dead Peer Detection action (nullable)
-	DPDAction *string `json:"dpdAction,omitempty"`
+	DPDAction *IKEDPDAction `json:"dpdAction,omitempty"`
 
 	// DPDInterval Dead Peer Detection interval
 	DPDInterval int32 `json:"dpdInterval,omitempty"`
